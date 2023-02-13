de167544f9f50b72b42b71a6999a017c63a8d564
thiagoleiteecarvalho <thiagolcarvalho81@gmail.com>
2023-02-13 14:15:42 -0300

    Add files via upload

---
+++ arvore/Arvore.py
@@ -0,0 +1,65 @@
+
+class No:
+    def __init__(self, dado: int, filho_esquerdo=None, filho_direito=None):
+        self.dado = dado
+        self.filho_esquerdo = filho_esquerdo
+        self.filho_direito = filho_direito
+
+
+def insert(no: No, dado: int):
+    
+    if dado <= no.dado:
+        if no.filho_esquerdo is None:
+            no.filho_esquerdo = No(dado)
+            return
+
+        insert(no.filho_esquerdo, dado)
+    else:
+        if no.filho_direito is None:
+            no.filho_direito = No(dado)
+            return
+
+        insert(no.filho_direito, dado)
+
+def delete(no: No, dado: int) -> Optional[No]:
+    
+    if no is None or no.dado == dado:
+        return None
+
+    if dado <= no.dado:
+        no.filho_esquerdo = delete(no.filho_esquerdo, dado)
+        return no
+
+    no.filho_direito = delete(no.filho_direito, dado)
+    return no
+
+def search(no: No, dado: int) -> Optional[No]:
+    
+    if no is None:
+        return print('Valor não pertence à ávore')
+
+    if no.dado == dado:
+        return print('Valor pertence à ávore')
+
+    if dado <= no.dado:
+        return search(no.filho_esquerdo, dado)
+
+    return search(no.filho_direito, dado)
+
+def imprimir_pre_ordem(no: No):
+
+    print(no.dado + ' ')
+    imprimir_pre_ordem(no.filho_esquerdo)
+    imprimir_pre_ordem(no.filho_direito)
+
+def imprimir_em_ordem(no: No):
+
+    imprimir_pre_ordem(no.filho_esquerdo)
+    print(no.dado + ' ')
+    imprimir_pre_ordem(no.filho_direito)
+
+def imprimir_pos_ordem(no: No):
+
+    imprimir_pre_ordem(no.filho_esquerdo)
+    imprimir_pre_ordem(no.filho_direito)
+    print(no.dado + ' ')
---
+++ arvore/ArvoreMain.py
@@ -0,0 +1,45 @@
+import arvore
+
+if __name__ == '__main__':
+    raiz: arvore.No = None
+    while True:
+        print('Selecione uma opção:')
+        print('1 - Criar raiz')
+        print('2 - INSERT')
+        print('3 - DELETE')
+        print('4 - SEARCH')
+        print('5 - Exibir Pré-ordem')
+        print('6 - Exibir Em-ordem')
+        print('7 - Exibir Pós-ordem')
+        print('8 - Sair')
+
+        opcao = int(input('Opção: '))
+        if opcao == 1:
+            print("Digite o valor(numérico e inteiro) da raiz: ", end='')
+            dado = int(input())
+            raiz = arvore.No(dado)
+        elif opcao == 2:
+            print("Digite o valor(numérico e inteiro) a ser inserido: ", end='')
+            dado = int(input())
+            arvore.insert(raiz, dado)
+        elif opcao == 3:
+            print("Digite o valor(numérico e inteiro) a ser excluído: ", end='')
+            dado = int(input())
+            arvore.delete(raiz, dado)
+        elif opcao == 4:
+            print("Digite o valor(numérico e inteiro) a ser pesquisado: ", end='')
+            dado = int(input())
+            arvore.search(raiz, dado)
+        elif opcao == 5:
+            print("Exibindo Pré-ordem")
+            arvore.imprimir_pre_ordem(raiz)
+        elif opcao == 6:
+            print("Exibindo Em-ordem")
+            arvore.imprimir_em_ordem(raiz)
+        elif opcao == 7:
+            print("Exibindo Pós-ordem")
+            arvore.imprimir_pos_ordem(raiz)
+        elif opcao == 8:
+            break
+        else:
+            print('Opção desconhecida!')
